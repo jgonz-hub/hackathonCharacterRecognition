d21b0d0c3e4fb4072fed8982b73f45649eb57532
Gonzalez <jgonz22@iastate.edu>
2023-09-30 14:20:17 -0500

    Initial website stuff

---
+++ app.py
@@ -0,0 +1,50 @@
+import os
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'png', 'gif'}
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        # Check if a file is uploaded
+        if 'file' not in request.files:
+            return redirect(request.url)
+
+        file = request.files['file']
+
+        if file.filename == '':
+            return redirect(request.url)
+
+        if file and allowed_file(file.filename):
+            # Save the uploaded file
+            filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+            file.save(filename)
+
+            # Perform image recognition here (not implemented in this example)
+
+            # Redirect to the result page with the image filename
+            return redirect(url_for('result', filename=file.filename))
+
+    return render_template('index.html')
+
+
+@app.route('/result/<filename>')
+def result(filename):
+    # Perform image recognition and character detection here
+    # You can pass the detected character as a variable to the template
+    detected_character = "SpongeBob SquarePants"  # Example, replace with actual detection result
+
+    return render_template('result.html', filename=filename, detected_character=detected_character)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
